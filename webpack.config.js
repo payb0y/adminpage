88f927113cb3090827ffac5725827829855f8de9
payb0y <othmane.gat@gmail.com>
2026-02-09 21:38:56 +0100

    Refactor webpack configuration by renaming entry point from 'adminpage-main' to 'main' and remove obsolete adminpage-main.js files along with their associated license and source map files.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
 webpackConfig.entry = {
-    'adminpage-main': path.join(__dirname, 'src', 'main.js'),
+    'main': path.join(__dirname, 'src', 'main.js'),
 }
 
 module.exports = webpackConfig
